d9eae61fc1771295337275d5be895427d32c5af4
ArtemChizevskikh <artem74ru@hotmail.com>
2020-05-09 18:38:20 +0500

    publish

---
+++ Makefile
@@ -0,0 +1,8 @@
+install:
+	npm install
+build:
+	NODE_ENV=production npx webpack
+test:
+	npm test
+lint:
+	npx eslint .
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import fs from 'fs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const run = () => {
+  const { body } = document;
+  const element = document.createElement('div');
+  body.append(element);
+};
+
+run();
